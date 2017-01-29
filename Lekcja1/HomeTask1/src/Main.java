d5fdecc4d46c8f4a5601410b0d9f5a4949ba0478
Maciuk85 <maciug85@gmail.com>
2017-01-29 14:43:18 +0100

    Część zadanie domowego

--- Lekcja1/HomeTask1/src/Main.java
+++ Lekcja1/HomeTask1/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
-
+		String word;
+		String ss= "aaa";
+		final char[] alphabet = { 'a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','w','x','y','z'};
+		char[] alphabet2 = new char[alphabet.length];
+		ArrayList<Character> vectorAlphabet = new ArrayList<Character>(); //alphabet,alphabet+alphabet.length
+		int operationNum; 
+		int a;
 }
+}
+
+	
